27dd4dfb4c713574e53c5ff3650ba0241c80fb1b
YongBum <59983323+Yong-Park@users.noreply.github.com>
2021-09-15 23:56:34 +0000

    Se comenzo a trabajar respecto al menu uno

--- main.cs
+++ main.cs
@@ -1,7 +1,26 @@
 using System;
 
 class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+
+  //primer menu
+  public static void PrimerMenu(){
+    //atributos
+    int seleccion_menu=0;
+    //opciones del primer menu
+    do{
+      Console.WriteLine("1. Registro");
+      Console.WriteLine("2. Iniciar simulacion");
+      Console.WriteLine("3. Valor de criptomonedas");
+      Console.WriteLine("4. Salir");
+      seleccion_menu=Convert.ToInt32(Console.ReadLine());
+
+    }while(seleccion_menu!=4);
   }
+
+  public static void Main (string[] args) {
+    PrimerMenu();
+    Console.WriteLine("Gracias, espero que vuelva pronto");
+  }
+
+  
 }
